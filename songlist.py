a8eea3ce332f70df50eec3ea0a7ae93d76f5cceb
wayykaard <paxananajun14@mail.ru>
2022-01-03 16:21:11 +0300

    Я сделал первую задачку со списком)

--- songlist.py
+++ songlist.py
@@ -1,7 +1,13 @@
-a = halo = '"Halo"'
-a = 4.9
-b = enjoy = '"Enjoy the Silence"'
-b = 4.20
-c = clean = '"Clean"'
-c = 5.83
-print("Вместе три песни звучат", a + b + c, "минут")
+violator_songs_dict = {
+    'World in My Eyes': 4.76,
+    'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.30,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.6,
+    'Policy of Truth': 4.88,
+    'Blue Dress': 4.18,
+    'Clean': 5.68,
+}
+x = violator_songs_dict['Halo'] + violator_songs_dict['Enjoy the Silence'] + violator_songs_dict['Clean']
+    print(f"Три песни, 'Halo', 'Enjoy the Silence' и 'Clean', вместе звучат {x} минут")
